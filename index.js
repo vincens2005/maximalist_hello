0a88aac6d3df175fdef63c28d076734e59ee58af
cukmekerb <cukmekerb@gmail.com>
2021-08-14 17:49:33 -0700

    add in express server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+var fetch = require("node-fetch");
+var express = require("express");
+var PORT = 8888;
+/* this server returns whatever you put after the slash */
+function setup_server() {
+	var app = express();
+	app.get("/:text", (req, res) => {
+		let text = req.params.text;
+		res.send(text);
+	});
+	app.listen(PORT);
+}
+
+async function print_from_server(text) {
+	let response = await fetch("http://localhost:" + PORT + "/" + text).then(a => a.text());
+	console.log(response);
+}
+
+async function main() {
+	setup_server();
+	let string_to_print = "hello world";
+	await print_from_server(string_to_print);
+	process.exit();
+}
+
+
+main();
--- readme.md
+++ readme.md
@@ -12,7 +12,7 @@ Any pull request that follows these guidelines will be merged:
 	2. it **only** uses nodeJS, no other languages
 	3. it only uses `index.js` and installs node modules
 	4. it uses yarn package management
-	5. no changes are made to the readme or license
+	5. no changes are made to any file other than `index.js` and `package.json`
 	6. it requires no external services (can run offline)
 	7. it makes the program more convoluted or does not affect the convolution levels (no optimizations, please)
 	8. it is pogr
